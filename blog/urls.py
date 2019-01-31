144f0c19233cf749e0a94b35c18a2ea46debffd8
dh1p0em <anilefecoban5@gmail.com>
2019-01-31 12:26:23 +0300

    nothing

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from . import views
+from django.urls import path # Yönlendirme işlemi için gerekli import
+from . import views # Yönlendirme views'in içinde import et 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'), # path(yönlenecek şeyler, view belirt, name)
 ]
